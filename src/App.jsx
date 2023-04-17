36e5cbc1f9c298d9e4a7e28f48ba7a67ce597881
Splagadou <benoit@vandanjon.fr>
2023-04-17 17:30:25 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/products" element={<Products />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,5 @@
+function Products(params) {
+	return <h1>Products</h1>;
+}
+
+export default Products;
